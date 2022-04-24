e7a3104c4f2a08eb3c24337c5b950bc0f6591e4d
Matbe_ <59774749+Matbe9@users.noreply.github.com>
2022-04-24 10:30:16 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -15,9 +15,10 @@ connect = client_socket.connect((host, port))
 print("Connected to the server")
 while True:
     # Send a message with #01:01 and the ram usage
-    cpu = f"#01:02,{int(psutil.cpu_percent(1))}".encode()
     ram = f"#01:01,{int(psutil.virtual_memory()[2])}".encode()
+    cpu = f"#01:02,{int(psutil.cpu_percent(1))}".encode()
     client_socket.send(cpu)
+    time.sleep(0.5)
     client_socket.send(ram)
 
     print("Message sent \nCPU:"+str(psutil.cpu_percent(4))+"%\nRAM:"+str(psutil.virtual_memory()[2])+"%")
--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import socket
 import threading
 import pymysql
 
-webhook_url = ""
+webhook_url = "https://discord.com/api/webhooks/962749706243547136/lzWeN79baiNo6lMkiUMHjLwWuWyxfQT-leg9Y90W_7QEdw8XPmscCpUCsGUh-cO-w1TO"
 
 # Make a threading class for the client.
 class ClientThread(threading.Thread):
@@ -25,12 +25,13 @@ class ClientThread(threading.Thread):
         while True:
 
             message = self.client_socket.recv(1024)
+            print(message.decode())
             # If the message is empty, we will close the connection.
             if not message:
                 break
 
             # We will use the MariaDB client to write the data to the database.
-            db = pymysql.connect(host="localhost", user="grafana", password="password(not the real password)", database="matbeSync")
+            db = pymysql.connect(host="localhost", user="grafana", password="JeSuisPasGay999__", database="matbeSync")
             cursor = db.cursor()
             # if messsage starts with '01:#01', we will insert the data into the mimas_ram table.
             if message.startswith('#01:01'.encode()):
@@ -58,8 +59,11 @@ class ClientThread(threading.Thread):
                     else:
                         pass
                 # We will insert the data into the database.
-                cursor.execute("INSERT INTO mimas_ram (ram) VALUES (%s)", (data[1].decode()))
-                db.commit()
+                try:
+                    cursor.execute("INSERT INTO mimas_ram (ram) VALUES (%s)", (int(data[1].decode())))
+                    db.commit()
+                except Exception as e:
+                    print(e)
 
             # if messsage starts with '#01:02', we will insert the data into the mimas_cpu table.
             if message.startswith('#01:02'.encode()):
@@ -91,8 +95,80 @@ class ClientThread(threading.Thread):
                 else:
                     ram_over_use = False
 
-                cursor.execute("INSERT INTO mimas_cpu (cpu) VALUES (%s)", (data[1].decode()))
-                db.commit()
+                try:
+                    cursor.execute("INSERT INTO mimas_cpu (cpu) VALUES (%s)", (int(data[1].decode())))
+                    db.commit()
+                except Exception as e:
+                    print(e)
+
+            # if messsage starts with '02:#01', we will insert the data into the paaliaq_ram table.
+            if message.startswith('#02:01'.encode()):
+                # We will split the message into the data.
+                data = message.split(','.encode())
+                if int(data[1].decode()) >= 85:
+                    if cpu_over_use == False:
+                        # We will send an embed message to the discord Webhook.
+                        # Create an embed object with a orange color and the title is "CPU usage warning". The description was "CPU usage is over 70%, exactly at " + data[1] + "%".
+                        embed = DiscordEmbed(title="Paaliaq RAM usage warning", color=0xFFA500,
+                                             description="RAM usage is over 85%, exactly at " + data[1].decode() + "%")
+                        # Send the embed message to the discord Webhook.
+                        webhook = DiscordWebhook(url=webhook_url, content="", embeds=[embed])
+                        webhook.execute()
+                    else:
+                        pass
+                elif int(data[1].decode()) >= 70:
+                    if cpu_over_use == False:
+                        # We will send an embed message to the discord Webhook.
+                        # Create an embed object with a orange color and the title is "CPU usage warning". The description was "CPU usage is over 70%, exactly at " + data[1] + "%".
+                        embed = DiscordEmbed(title="Paaliaq RAM usage warning", color=0xFFA500, description="RAM usage is over 70%, exactly at " + data[1].decode() + "%")
+                        # Send the embed message to the discord Webhook.
+                        webhook = DiscordWebhook(url=webhook_url, content="", embeds=[embed])
+                        webhook.execute()
+                    else:
+                        pass
+                # We will insert the data into the database.
+                try:
+                    cursor.execute("INSERT INTO paaliaq_ram (ram) VALUES (%s)", (int(data[1].decode())))
+                    db.commit()
+                except Exception as e:
+                    print(e)
+
+            # if messsage starts with '#02:02', we will insert the data into the paaliaq_cpu table.
+            if message.startswith('#02:02'.encode()):
+                # We will split the message into the data.
+                data = message.split(','.encode())
+                if int(data[1].decode()) >= 85:
+                    if ram_over_use == False:
+                        # We will send an embed message to the discord Webhook.
+                        # Create an embed object with a orange color and the title is "CPU usage warning". The description was "CPU usage is over 70%, exactly at " + data[1] + "%".
+                        embed = DiscordEmbed(title="Paaliaq CPU usage warning", color=0xFFA500,description="CPU usage is over 85%, exactly at " + data[1].decode() + "%")
+                        # Send the embed message to the discord Webhook.
+                        webhook = DiscordWebhook(url=webhook_url, content="", embeds=[embed])
+                        webhook.execute()
+                    else:
+                        pass
+
+                elif int(data[1].decode()) >= 70:
+                    if ram_over_use == False:
+                        # We will send an embed message to the discord Webhook.
+                        # Create an embed object with a orange color and the title is "CPU usage warning". The description was "CPU usage is over 70%, exactly at " + data[1] + "%".
+                        embed = DiscordEmbed(title="Paaliaq CPU usage warning", color=0xFFA500,description="CPU usage is over 70%, exactly at " + data[1].decode() + "%")
+                        # Send the embed message to the discord Webhook.
+                        webhook = DiscordWebhook(url=webhook_url, content="", embeds=[embed])
+                        webhook.execute()
+                        ram_over_use = True
+                    else:
+                        pass
+
+                else:
+                    ram_over_use = False
+
+                try:
+                    cursor.execute("INSERT INTO paaliaq_cpu (cpu) VALUES (%s)", (int(data[1].decode())))
+                    db.commit()
+                except Exception as e:
+                    print(e)
+
 # Create a socket object
 s = socket.socket()
 # Bind to the port
